3b3055db700e107baa8cbcc39cc04bd925d9fcac
Rodrigo Zapiello <rzapiello@nexocs.com>
2024-08-03 15:44:57 -0300

    Finalização do projeto

---
+++ galeria/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def index(request):
+    return render(request, 'galeria/index.html')
+# Create your views here.
+
+def imagem(request):
+    return render(request, 'galeria/imagem.html')
+# Create your views here.
